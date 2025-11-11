4057ff68f8748c4164a6652b54daafe0cf0957f1
Delana Ryan <dryan15@teach-node-04.cs.mcgill.ca>
2025-11-11 16:50:19 -0500

    Basic eval function

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -46,4 +46,47 @@ class StudentAgent(Agent):
     # Dummy return (you should replace this with your actual logic)
     # Returning a random valid move as an example
     return random_move(chess_board,player)
+  
+    def eval(self, board, color, opponent):
+        # Simple evaluation function: difference in number of pieces
+        player_count = np.count_nonzero(board == color)
+        opp_count = np.count_nonzero(board == opponent)
+        score_diff = player_count - opp_count
 
+        # Add some score penalization for holes in position
+        # board[r][c] == 0 indicates empty square
+
+        hole_penalty = 0
+        n = board.shape[0] # board.shape[0] indicates board size
+
+        one_tile_dirs = get_directions() # Get all directions (8 directions: up, down, left, right, and diagonals), returns list of tuples
+        two_tile_dirs = get_two_tile_directions() # Get all 2-tile directions (16 directions), returns list of tuples
+        all_dirs = one_tile_dirs + two_tile_dirs
+
+        # TODO: I am pretty sure these loops can be optimized using memoization 
+        for row in range(n):
+          for col in range(n):
+              
+              if board[row][col] != 0:
+                  continue # Skip the rest of the inner loop if not an empty tile, DON'T USE BREAK HERE!
+                  #Continue goes to next iteration of inner loop, break would exit the entire loop
+
+              near_player = False
+              near_opp = False
+
+              for row_distance, col_distance in all_dirs: #For all direction vectors representing move
+                  new_row, new_col = row + row_distance, col + col_distance 
+                  if 0 <= new_row < n and 0 <= new_col < n: # As long as we are within bounds...
+                      if board[new_row][new_col] == color: 
+                          near_player = True
+                      elif board[new_row][new_col] == opponent:
+                          near_opp = True
+
+              # TODO: I am not sure if this considers whose turn it is... might need to adjust
+              # I think this is fine, because we will only call this function in mimimax
+              if near_opp and not near_player: # If move is reachable  by opponent but not by player
+                  hole_penalty += 1
+
+        # TODO: We can add more heuristics here, including a preference for corners, edges, etc.
+        # TODO: Modify the weights as needed, this can be done after testing. 
+        return score_diff - hole_penalty
