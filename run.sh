0c89435920c2be68d60f3dc5230b8d4086514c25
Delana <118754893+delanaryan@users.noreply.github.com>
2025-11-21 16:49:30 -0500

    more boards in run script

--- run.sh
+++ run.sh
@@ -10,4 +10,12 @@ do
     echo "" >> "$OUTPUT"
 done
 
+python ./simulator.py --player_1 greedy_corners_agent --player_2 student_agent --board_path boards/the_circle.csv >> "$OUTPUT"
+python ./simulator.py --player_1 greedy_corners_agent --player_2 student_agent --board_path boards/big_x.csv >> "$OUTPUT"
+python ./simulator.py --player_1 greedy_corners_agent --player_2 student_agent --board_path boards/plus1.csv >> "$OUTPUT"
+python ./simulator.py --player_1 greedy_corners_agent --player_2 student_agent --board_path boards/plus2.csv >> "$OUTPUT"
+python ./simulator.py --player_1 greedy_corners_agent --player_2 student_agent --board_path boards/plus1.csv >> "$OUTPUT"
+python ./simulator.py --player_1 greedy_corners_agent --player_2 student_agent --board_path boards/the_wall.csv >> "$OUTPUT"
+python ./simulator.py --player_1 greedy_corners_agent --player_2 student_agent --board_path boards/watch_the_sides.csv >> "$OUTPUT"
+
 echo "Done! Output saved to $OUTPUT"
